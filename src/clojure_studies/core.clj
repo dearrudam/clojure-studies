17713c8f9e77ba7b828cacca58b02705302da3c3
Maximillian Arruda <marruda@opentext.com>
2019-09-16 17:58:02 -0300

    You`re doing great!

--- src/clojure_studies/core.clj
+++ src/clojure_studies/core.clj
@@ -1,7 +1,74 @@
-(ns clojure-studies.core
-  (defn foo
-    "A foo function"
-    [x y]
-    (+ x y)))
+(ns clojure-studies.core)
 
-(foo 1 1)
+(defn foo
+  "A foo function"
+  [x y] (+ x y))
+
+
+;;
+;; Functions tests
+;;=============================================================================================
+;;
+;; 1) Define a function greet that takes no arguments and prints "Hello". Replace the with the implementation: 
+;; (defn greet [] )
+
+(defn greet []
+  (println "Hello"))
+;;
+;;
+;;=============================================================================================
+;;
+;;2) Redefine greet using def, first with the fn special form and then with the #() reader macro.
+
+;; using fn
+(def greetfn
+  (fn [] (println "Hello")))
+
+;; using #()
+(def greet#
+  #(println "Hello"))
+;;
+;;
+;;=============================================================================================
+;;3) Define a function greeting which:
+;;
+;;Given no arguments, returns "Hello, World!"
+;;
+;;Given one argument x, returns "Hello, x!"
+;;
+;;Given two arguments x and y, returns "x, y!"
+;;
+;; Hint use the str function to concatenate strings
+;;(doc str)
+(defn greeting
+  ([] (greeting "Hello" "World"))
+  ([x] (greeting "Hello" x))
+  ([x,y] (str x ", " y "!")))
+;;
+;;
+;;=============================================================================================
+;;4) Define a function do-nothing which takes a single argument x and returns it, unchanged.
+(defn do-nothing [x] x)
+;;
+;;
+;;=============================================================================================
+;;5) Define a function always-thing which takes any number of arguments, ignores all of them, and returns the number 100.
+(defn always-thing [& args] 100)
+;;
+;;
+;;=============================================================================================
+;;6) Define a function make-thingy which takes a single argument x. It should return another function, which takes any number of arguments and always returns x.
+(defn make-thingy
+  [x] (fn [& args] x))
+;;
+;;
+;;=============================================================================================
+;;7) Define a function triplicate which takes another function and calls it three times, without any arguments.
+(defn triplicate
+  [f] (f) (f) (f))
+;;
+;;
+;;=============================================================================================
+;;8) Define a function opposite which takes a single argument f. It should return another function which takes any number of arguments, applies f on them, and then calls not on the result. The not function in Clojure does logical negation.
+(defn opposite [f]
+  (fn [& args] (not (apply f args))))
--- test/clojure_studies/core_test.clj
+++ test/clojure_studies/core_test.clj
@@ -2,6 +2,29 @@
   (:require [clojure.test :refer :all]
             [clojure-studies.core :refer :all]))
 
-(deftest a-test
+(clojure.test/deftest testing-foo
   (testing "invalid result"
-    (is (= (foo 1 1) 2))))
+    (is (= (clojure-studies.core/foo 1 1) 2))))
+
+(clojure.test/deftest testing-greeting
+  (testing "invalid result"
+    (assert (= "Hello, World!" (clojure-studies.core/greeting)))
+    (assert (= "Hello, Clojure!" (clojure-studies.core/greeting "Clojure")))
+    (assert (= "Good morning, Clojure!" (clojure-studies.core/greeting "Good morning" "Clojure")))))
+
+(clojure.test/deftest testing-do-nothing
+  (testing "invalid result"
+    (assert (= 1 (clojure-studies.core/do-nothing 1)))))
+
+(clojure.test/deftest testing-always-thing
+  (testing "invalid result"
+    (assert (= 100 (clojure-studies.core/always-thing 1 2 3 4)))))
+
+
+(clojure.test/deftest testing-make-thingy
+  (testing "invalid result"
+    (let [n (rand-int Integer/MAX_VALUE)
+          f (clojure-studies.core/make-thingy n)]
+      (assert (= n (f)))
+      (assert (= n (f 123)))
+      (assert (= n (apply f 123 (range)))))))
